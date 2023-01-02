a164849256af675249c13cc0eaedc157f3861dca
Erdinch <erdinchorhan02@gmail.com>
2023-01-02 19:55:36 +0200

    Initial commit

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+  },
+  ignorePatterns: ['tailwind.config.cjs', 'postcss.config.cjs'],
+};
