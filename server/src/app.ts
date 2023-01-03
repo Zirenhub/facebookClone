3c457960aeed1241ad267681e93a530412f56485
Erdinch <erdinchorhan02@gmail.com>
2023-01-03 15:16:52 +0200

    set up mongoose

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,21 @@
 import express, { Express, Request, Response } from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+const port = process.env.PORT || 5000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+app.get('/api/v1', (req: Request, res: Response) => {
+  res.json({ status: 'success', data: null, message: null });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+mongoose.connect(process.env.DB_URI!).then(() => {
+  app.listen(port, () =>
+    console.log(`Server is running at http://localhost:${port}`)
+  );
 });
