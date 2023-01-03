5349a23ef51e460ec45f58ea80fb93f5171b6c18
Erdinch <erdinchorhan02@gmail.com>
2023-01-03 15:16:33 +0200

    set up proxy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api/v1': `http://localhost:${process.env.PORT}`,
+    },
+  },
   plugins: [react()],
-})
+});
