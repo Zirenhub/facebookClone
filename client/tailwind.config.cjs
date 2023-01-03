4e6e1118834a10ce15a5660da357744479ef532d
Erdinch <erdinchorhan02@gmail.com>
2023-01-03 17:54:24 +0200

    styling / design

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,31 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '480px',
+        ss: '620px',
+        sm: '768px',
+        md: '1060px',
+        lg: '1200px',
+        xl: '1700px',
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+        '6xl': '4.632rem',
+        '7xl': '5.232rem',
+        '8xl': '6.132rem',
+      },
+      backgroundImage: {
+        'facebook-logo': "url('/src/assets/facebook-logo.svg')",
+      },
+    },
   },
   plugins: [],
 };
