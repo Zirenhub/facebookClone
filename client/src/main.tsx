d96eeac2fa28be3dc390fa1e00077f704b0a5585
Erdinch <erdinchorhan02@gmail.com>
2023-01-03 17:53:52 +0200

    router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AuthPage from './components/AuthPage/AuthPage';
+import ErrorPage from './components/ErrorPage/ErrorPage';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AuthPage />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
