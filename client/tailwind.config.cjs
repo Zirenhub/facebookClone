12c8a563f29e6cbb1c84d8fc163dfbc401b3b688
Erdinch <erdinchorhan02@gmail.com>
2023-01-03 21:35:57 +0200

    tailwind config

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -3,10 +3,13 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        background: '#f0f2f5',
+      },
       screens: {
         xs: '480px',
         ss: '620px',
-        sm: '768px',
+        sm: '900px',
         md: '1060px',
         lg: '1200px',
         xl: '1700px',
@@ -23,8 +26,12 @@ module.exports = {
         '7xl': '5.232rem',
         '8xl': '6.132rem',
       },
+      fontFamily: {
+        ubuntu: ['Ubuntu'],
+      },
       backgroundImage: {
         'facebook-logo': "url('/src/assets/facebook-logo.svg')",
+        close: "url('/src/assets/x.svg')",
       },
     },
   },
