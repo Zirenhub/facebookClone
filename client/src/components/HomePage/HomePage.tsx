d568d56d3ce5dfb2258d97b42da50f9e416fb33b
Erdinch <erdinchorhan02@gmail.com>
2023-01-08 20:52:19 +0200

    homepage

---
+++ client/src/components/HomePage/HomePage.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <p>Hello</p>;
+}
+
+export default HomePage;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AuthPage from './components/AuthPage/AuthPage';
 import ErrorPage from './components/ErrorPage/ErrorPage';
+import HomePage from './components/HomePage/HomePage';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthPage />,
-    errorElement: <ErrorPage />,
+  },
+  {
+    path: '/homepage',
+    element: <HomePage />,
+    errorElement: <ErrorPage />, // can be used on dynamically routed pages
   },
 ]);
 
