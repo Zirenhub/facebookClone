aeb8952ab756cc4f943b59996b715b2b7146fb38
Erdinch <erdinchorhan02@gmail.com>
2023-01-09 17:34:00 +0200

    HomePage

--- client/src/components/HomePage/HomePage.tsx
+++ client/src/components/HomePage/HomePage.tsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from 'react';
+import MobileHeader from './MobileHeader';
+import MobileWritePost from './MobileWritePost';
+
 function HomePage() {
-  return <p>Hello</p>;
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const isMobile = width <= 500;
+
+  useEffect(() => {
+    function handleWindowChange() {
+      setWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', handleWindowChange);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowChange);
+    };
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className="flex flex-col">
+        <MobileHeader />
+        <MobileWritePost />
+      </div>
+    );
+  }
+  return <p>Hello desktop user</p>;
 }
 
 export default HomePage;
