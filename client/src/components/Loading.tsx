a8c32e2e4d765567700d7060329538338c12c3ef
Erdinch <erdinchorhan02@gmail.com>
2023-01-14 19:54:42 +0200

    loading screen

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-gray-600">
+      <p className="text-white">Loading...</p>
+    </div>
+  );
+}
+
+export default Loading;
