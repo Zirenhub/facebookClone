948358810f4b15b29aae5d54aafd9164512bcf13
Erdinch <erdinchorhan02@gmail.com>
2023-01-15 18:41:13 +0200

    finished header

--- client/src/components/HomePage/Mobile/Header.tsx
+++ client/src/components/HomePage/Mobile/Header.tsx
@@ -1,8 +1,16 @@
 import facebookLogo from '../../../assets/facebook-logo.svg';
+import search from '../../../assets/search.svg';
+import menu from '../../../assets/menu-bars.svg';
+import home from '../../../assets/home.svg';
+import friends from '../../../assets/friend.svg';
+import watch from '../../../assets/watch.svg';
+import groups from '../../../assets/groups.svg';
+import messenger from '../../../assets/messenger.svg';
+import bell from '../../../assets/bell.svg';
 
 function Header() {
   return (
-    <header className="max-h-24 pb-3 border-b-4 border-slate-400">
+    <header className="max-h-24 pb-3 border-b-2 border-slate-400">
       <div className="flex">
         <div className="grow flex justify-start">
           <img
@@ -11,18 +19,34 @@ function Header() {
             src={facebookLogo}
           />
         </div>
-        <div className="flex gap-5 p-2">
-          <div className="w-[30px] h-[30px] bg-gray-300" />
-          <div className="w-[30px] h-[30px] bg-gray-300" />
+        <div className="flex gap-2 px-2 pt-1 items-center">
+          <div className="bg-gray-200 w-9 h-9 p-1 rounded-full">
+            <img src={search} alt="search" className="block m-auto" />
+          </div>
+          <div className="bg-gray-200 w-9 h-9 p-1 rounded-full">
+            <img src={menu} alt="menu" className="block m-auto" />
+          </div>
         </div>
       </div>
-      <div className="flex justify-between px-5">
-        <div className="w-[30px] h-[30px] bg-gray-300" />
-        <div className="w-[30px] h-[30px] bg-gray-300" />
-        <div className="w-[30px] h-[30px] bg-gray-300" />
-        <div className="w-[30px] h-[30px] bg-gray-300" />
-        <div className="w-[30px] h-[30px] bg-gray-300" />
-        <div className="w-[30px] h-[30px] bg-gray-300" />
+      <div className="flex justify-between pt-2 items-center">
+        <div className="grow">
+          <img src={home} alt="home" className="block m-auto" />
+        </div>
+        <div className="grow">
+          <img src={friends} alt="friends" className="block m-auto" />
+        </div>
+        <div className="grow">
+          <img src={messenger} alt="messenger" className="block m-auto" />
+        </div>
+        <div className="grow">
+          <img src={watch} alt="watch" className="block m-auto" />
+        </div>
+        <div className="grow">
+          <img src={bell} alt="notifications" className="block m-auto" />
+        </div>
+        <div className="grow">
+          <img src={groups} alt="groups" className="block m-auto" />
+        </div>
       </div>
     </header>
   );
