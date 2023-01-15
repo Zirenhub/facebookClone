43f7deb2c2ec0aa4e31ecbcb13d827508b4a457e
Erdinch <erdinchorhan02@gmail.com>
2023-01-15 16:54:14 +0200

    create post modal

---
+++ client/src/components/HomePage/Mobile/CreatePost.tsx
@@ -0,0 +1,9 @@
+function CreatePost() {
+  return (
+    <div className="z-10 absolute bg-gray-300/30 top-0 left-0 h-full w-full">
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default CreatePost;
--- client/src/components/HomePage/Mobile/WritePost.tsx
+++ client/src/components/HomePage/Mobile/WritePost.tsx
@@ -1,8 +1,20 @@
+import { lazy, Suspense, useState } from 'react';
 import pfp from '../../../assets/pfp.svg';
+import Loading from '../../Loading';
+
+const CreatePostModal = lazy(() => import('./CreatePost'));
 
 function WritePost() {
+  const [isCreatePostModalOpen, setIsCreatePostModalOpen] =
+    useState<boolean>(false);
+
   return (
     <div className="flex items-center px-2 py-4">
+      {isCreatePostModalOpen && (
+        <Suspense fallback={<Loading />}>
+          <CreatePostModal />
+        </Suspense>
+      )}
       <div>
         <img src={pfp} alt="profile" className="max-w-[48px]" />
       </div>
@@ -10,6 +22,7 @@ function WritePost() {
         <button
           type="button"
           className="px-5 py-2 min-w-full rounded-full bg-gray-200 text-start"
+          onClick={() => setIsCreatePostModalOpen(true)}
         >
           What&apos;s on your mind?
         </button>
