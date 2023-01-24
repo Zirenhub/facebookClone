fffea5a7ac7d31f753364f0f3e1f50fe6c5c343a
Erdinch <erdinchorhan02@gmail.com>
2023-01-24 21:58:28 +0200

    testing tanstack

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -1,7 +1,26 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
+import getProfile from '../api/profile';
+import Loading from '../components/Loading';
 
 function ProfilePage() {
   const { id } = useParams();
+  const queryClient = useQueryClient();
+
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ['profile', id],
+    queryFn: () => getProfile(id!), // id could be undefined, some typescript problems
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isError) {
+    return <p>404</p>;
+  }
 
   return (
     <div>
