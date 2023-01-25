a1fdbf52e6bfdf61209eb6e7dae0eabce422447d
Erdinch <erdinchorhan02@gmail.com>
2023-01-25 22:43:09 +0200

    follow profile // todo

--- server/src/controllers/profileController.ts
+++ server/src/controllers/profileController.ts
@@ -1,5 +1,5 @@
 import { Response } from 'express';
-import { Types } from 'mongoose';
+import mongoose from 'mongoose';
 import { IUserRequest } from '../middleware/jwtAuth';
 import ProfileModel from '../models/profile';
 
@@ -16,7 +16,39 @@ export const getProfile = async (req: IUserRequest, res: Response) => {
     }
     throw new Error("Profile doesn't exist");
   } catch (err) {
-    res.status(400).json({
+    res.status(404).json({
+      status: 'error',
+      errors: err,
+      message: null,
+    });
+  }
+};
+
+export const followProfile = async (req: IUserRequest, res: Response) => {
+  try {
+    const id = new mongoose.Types.ObjectId(req.params.id);
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res
+        .status(404)
+        .json({ status: 'error', errors: 'Invalid ID', message: null });
+    }
+
+    if (req.user._id === id) {
+      return res
+        .status(400)
+        .json({
+          status: 'error',
+          errors: 'You cannot follow yourself',
+          message: null,
+        });
+    }
+
+    const profile = await ProfileModel.findById(id);
+    if (profile) {
+    }
+    throw new Error("Profile doesn't exist");
+  } catch (err) {
+    res.status(500).json({
       status: 'error',
       errors: err,
       message: null,
--- server/src/routes/profileRoute.ts
+++ server/src/routes/profileRoute.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import { jwtAuth } from '../middleware/jwtAuth';
-import { getProfile } from '../controllers/profileController';
+import { getProfile, followProfile } from '../controllers/profileController';
 
 const router = express.Router();
 
 router.get('/:id', jwtAuth, getProfile);
+router.get('/:id/follow', jwtAuth, followProfile);
 
 export default router;
