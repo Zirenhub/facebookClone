021d41aab23bb9b276622569da9b93920a2d362b
Erdinch <erdinchorhan02@gmail.com>
2023-01-25 22:40:07 +0200

    get profile, add fullname

--- client/src/api/profile.ts
+++ client/src/api/profile.ts
@@ -1,8 +1,13 @@
+import getFullName from '../components/utils/getFullName';
+
 export default async function getProfile(id: string) {
   const res = await fetch(`/api/v1/profile/${id}`);
   const resData = await res.json();
   if (resData.status === 'success') {
-    return resData.data;
+    return {
+      ...resData.data,
+      fullName: getFullName(resData.data.firstName, resData.data.lastName),
+    };
   }
   throw new Error('Page is not valid');
 }
