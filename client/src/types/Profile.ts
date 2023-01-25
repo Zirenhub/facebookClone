b7e2c383a49e405a4ec14e8583b006ecf8be23fb
Erdinch <erdinchorhan02@gmail.com>
2023-01-25 17:46:22 +0200

    profile type

---
+++ client/src/types/Profile.ts
@@ -0,0 +1,11 @@
+export type TProfile = {
+  __v: string;
+  _id: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  gender: string;
+  customGender?: string;
+  createdAt: string;
+  updatedAt: string;
+};
