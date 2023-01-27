8b490dc005a4567465499884c5edbe64a2653772
Erdinch <erdinchorhan02@gmail.com>
2023-01-27 17:58:32 +0200

    redesigned

--- client/src/components/HomePage/Mobile/WritePost.tsx
+++ client/src/components/HomePage/Mobile/WritePost.tsx
@@ -1,36 +1,32 @@
 import { lazy, Suspense, useState } from 'react';
 import pfp from '../../../assets/pfp.svg';
 import pictures from '../../../assets/pictures.svg';
-import feeling from '../../../assets/feeling.svg';
-import camera from '../../../assets/camera.svg';
-import location from '../../../assets/location.svg';
 import Loading from '../../Loading';
 
 const CreatePostModal = lazy(() => import('./CreatePost'));
 
 function WritePost() {
-  const [isCreatePostModalOpen, setIsCreatePostModalOpen] =
-    useState<boolean>(false);
+  const [openCreatePost, setOpenCreatePost] = useState<boolean>(false);
 
-  if (isCreatePostModalOpen) {
+  if (openCreatePost) {
     return (
       <Suspense fallback={<Loading />}>
-        <CreatePostModal close={() => setIsCreatePostModalOpen(false)} />
+        <CreatePostModal close={() => setOpenCreatePost(false)} />
       </Suspense>
     );
   }
 
   return (
     <div className="flex flex-col">
-      <div className="flex items-center px-2 py-4">
+      <div className="flex items-center p-2">
         <div>
           <img src={pfp} alt="profile" className="w-pfp" />
         </div>
         <div className="grow px-3">
           <button
             type="button"
-            className="px-5 py-2 min-w-full rounded-full bg-gray-200 text-start"
-            onClick={() => setIsCreatePostModalOpen(true)}
+            className="px-5 py-2 min-w-full rounded-full border-2 border-gray-300 text-start"
+            onClick={() => setOpenCreatePost(true)}
           >
             What&apos;s on your mind?
           </button>
@@ -40,24 +36,6 @@ function WritePost() {
             <img src={pictures} alt="photos" className="h-[32px]" />
           </div>
           <p className="text-sm">Photo</p>
-        </div>
-      </div>
-      <div className="flex justify-around pb-3">
-        <div className="flex items-center gap-2 cursor-pointer">
-          <img src={feeling} alt="feeling" className="block m-auto w-[24px]" />
-          <p>Feeling</p>
-        </div>
-        <div className="flex items-center gap-2 cursor-pointer">
-          <img src={camera} alt="video" className="block m-auto w-[21px]" />
-          <p>Video</p>
-        </div>
-        <div className="flex items-center gap-2 cursor-pointer">
-          <img
-            src={location}
-            alt="location"
-            className="block m-auto w-[21px]"
-          />
-          <p>Location</p>
         </div>
       </div>
     </div>
--- client/src/components/ProfilePages/ProfileHeader.tsx
+++ client/src/components/ProfilePages/ProfileHeader.tsx
@@ -1,22 +1,10 @@
-import { useNavigate } from 'react-router-dom';
-import BackButton from '../utils/BackButton';
 import pfp from '../../assets/pfp.svg';
 
 function ProfileHeader({ fullName }: { fullName: string }) {
-  const navigate = useNavigate();
-
   return (
-    <header className="flex flex-col border-b-2 mb-20">
-      <div className="flex p-2">
-        <BackButton
-          close={() => {
-            navigate('/home');
-          }}
-        />
-        <p className="font-bold">{fullName}</p>
-      </div>
+    <header className="flex flex-col relative border-b-2 mb-20">
       <div className="w-full h-36 bg-gray-300" />
-      <div className="absolute flex flex-col top-24 pl-2">
+      <div className="absolute flex flex-col top-16 pl-2">
         <img
           src={pfp}
           alt="profile"
