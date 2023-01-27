23d319abee56c5f85c7edb3b44d121bba13d90b5
Erdinch <erdinchorhan02@gmail.com>
2023-01-27 17:59:44 +0200

    ProfilePage fix for now

--- client/src/pages/ProfilePage.tsx
+++ client/src/pages/ProfilePage.tsx
@@ -5,12 +5,11 @@ import OwnProfile from '../components/ProfilePages/OwnProfile';
 import StrangerProfile from '../components/ProfilePages/StrangerProfile';
 import Loading from '../components/Loading';
 import useAuthContext from '../hooks/useAuthContext';
-import getProfile from '../api/profile';
+import { getProfile } from '../api/profile';
 
-function ProfilePage() {
+function ProfilePage({ isMobile }: { isMobile: boolean }) {
   const { id } = useParams();
   const auth = useAuthContext();
-  const isMobile = window.innerWidth <= 500;
 
   const { isLoading, isError, data, error } = useQuery<TProfile, Error>({
     queryKey: ['profile', id],
@@ -33,10 +32,12 @@ function ProfilePage() {
   }
 
   if (data._id === auth.user?._id) {
-    return isMobile ? <OwnProfile data={data} /> : '';
+    // return isMobile ? <OwnProfile data={data} /> : '';
+    return <OwnProfile data={data} />;
   }
 
-  return isMobile ? <StrangerProfile data={data} /> : '';
+  // return isMobile ? <StrangerProfile data={data} /> : '';
+  return <StrangerProfile data={data} />;
 }
 
 export default ProfilePage;
