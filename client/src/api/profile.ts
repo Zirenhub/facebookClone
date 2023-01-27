65c4ddbae72bb738f834112bcefeff5c13c753a6
Erdinch <erdinchorhan02@gmail.com>
2023-01-27 17:57:06 +0200

    api profile

--- client/src/api/profile.ts
+++ client/src/api/profile.ts
@@ -1,6 +1,10 @@
 import getFullName from '../components/utils/getFullName';
 
-export default async function getProfile(id: string) {
+function getError(data: any) {
+  return data.message ? data.message : data.errors.message;
+}
+
+export async function getProfile(id: string) {
   const res = await fetch(`/api/v1/profile/${id}`);
   const resData = await res.json();
   if (resData.status === 'success') {
@@ -9,5 +13,23 @@ export default async function getProfile(id: string) {
       fullName: getFullName(resData.data.firstName, resData.data.lastName),
     };
   }
-  throw new Error('Page is not valid');
+  throw new Error(getError(resData));
+}
+
+export async function followProfile(id: string) {
+  const res = await fetch(`/api/v1/profile/${id}/follow`);
+  const resData = await res.json();
+  if (resData.status === 'success') {
+    return resData.data;
+  }
+  throw new Error(getError(resData));
+}
+
+export async function followersProfile(id: string) {
+  const res = await fetch(`/api/v1/profile/${id}/followers`);
+  const resData = await res.json();
+  if (resData.status === 'success') {
+    return resData.data;
+  }
+  throw new Error(getError(resData));
 }
