e6678f7d8cf32da081a7f8d15cc692a06b008528
Erdinch <erdinchorhan02@gmail.com>
2023-01-29 20:32:36 +0200

    Request type

---
+++ client/src/types/Requests.ts
@@ -0,0 +1,11 @@
+import { TProfile } from './Profile';
+
+export type TRequest = {
+  __v: string;
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+  friend: TProfile;
+  profile: string;
+  status: string;
+};
