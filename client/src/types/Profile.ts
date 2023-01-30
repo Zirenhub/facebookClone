0124ca94218647eb0ea74bb190b08d81ecc3f03b
Erdinch <erdinchorhan02@gmail.com>
2023-01-30 21:37:21 +0200

    profile type w/o full name

--- client/src/types/Profile.ts
+++ client/src/types/Profile.ts
@@ -10,3 +10,15 @@ export type TProfile = {
   createdAt: string;
   updatedAt: string;
 };
+
+export type TProfileWithoutFullName = {
+  __v: string;
+  _id: string;
+  firstName: string;
+  lastName: string;
+  birthday: string;
+  gender: string;
+  customGender?: string;
+  createdAt: string;
+  updatedAt: string;
+};
