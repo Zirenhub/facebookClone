76936db1bbf1cd2c8d23afe651119235cdeeb323
Erdinch <erdinchorhan02@gmail.com>
2023-01-31 21:31:04 +0200

    refetch requests on accept or reject

--- client/src/components/Friends/FriendsRequests.tsx
+++ client/src/components/Friends/FriendsRequests.tsx
@@ -1,9 +1,16 @@
+import { QueryObserverResult } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
-import { TRequest } from '../../types/Requests';
+import { TRequest } from '../../types/Request';
 import Pfp from '../../assets/pfp-two.svg';
 import { acceptRequest, rejectRequest } from '../../api/profile';
 
-function FriendsRequests({ data }: { data: TRequest[] }) {
+function FriendsRequests({
+  data,
+  refetch,
+}: {
+  data: TRequest[];
+  refetch: () => Promise<QueryObserverResult>;
+}) {
   const navigate = useNavigate();
 
   async function handleRequest(reqID: string, accept: boolean) {
@@ -13,6 +20,8 @@ function FriendsRequests({ data }: { data: TRequest[] }) {
       } else {
         await rejectRequest(reqID);
       }
+
+      await refetch();
     } catch (err) {
       console.log(err);
     }
@@ -24,7 +33,7 @@ function FriendsRequests({ data }: { data: TRequest[] }) {
         return (
           <div
             key={request._id}
-            className="flex justify-between bg-gray-200 rounded-lg p-2"
+            className="flex justify-between bg-gray-200 rounded-lg p-2 shadow-md"
           >
             <button
               type="button"
