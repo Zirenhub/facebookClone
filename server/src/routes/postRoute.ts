3c368d9c85efcb8befcef7c65f2cce4a66f517c7
Erdinch <erdinchorhan02@gmail.com>
2023-02-08 21:34:46 +0200

    multer in router

--- server/src/routes/postRoute.ts
+++ server/src/routes/postRoute.ts
@@ -1,14 +1,16 @@
 import express from 'express';
 import { jwtAuth } from '../middleware/jwtAuth';
+import multer from 'multer';
 import { createPost, getPost } from '../controllers/postController';
 
 const router = express.Router();
+const upload = multer();
 
-router.post('/', jwtAuth, createPost);
+router.post('/', jwtAuth, upload.single('image'), createPost);
 router.get('/:id', jwtAuth, getPost);
 
 // update post ?
 
-router.post('/delete/:id'); // delete post
+router.post('/:id/delete'); // delete post
 
 export default router;
