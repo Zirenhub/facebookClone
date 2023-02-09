ee395ba0e395334938c4ee368a1851bbdddac16b
Erdinch <erdinchorhan02@gmail.com>
2023-02-09 21:08:25 +0200

    combined posts types

--- client/src/types/Post.ts
+++ client/src/types/Post.ts
@@ -5,25 +5,17 @@ export type TPost = {
   audience: string;
 };
 
-export type ImagePost = {
+export type TDBPost = {
   __v: string;
   _id: string;
   author: string;
   audience: string;
   content: string;
-  image: Blob;
-  reactions: [];
-  createdAt: string;
-  updatedAt: string;
-};
-
-export type DefaultPost = {
-  __v: string;
-  _id: string;
-  author: string;
-  audience: string;
-  content: string;
-  background: string | null;
+  image?: {
+    data: BufferType;
+    contentType: string;
+  };
+  background?: string | null;
   reactions: [];
   createdAt: string;
   updatedAt: string;
