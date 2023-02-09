11a1b0490c54b76be2a99d503160e92abfdb8e42
Erdinch <erdinchorhan02@gmail.com>
2023-02-09 17:57:08 +0200

    Post types

--- client/src/types/Post.ts
+++ client/src/types/Post.ts
@@ -1,6 +1,30 @@
 export type TPost = {
   content: string;
-  background?: string | null;
-  image?: File | null;
+  background: string | null;
+  image: File | null;
   audience: string;
+};
+
+export type ImagePost = {
+  __v: string;
+  _id: string;
+  author: string;
+  audience: string;
+  content: string;
+  image: Blob;
+  reactions: [];
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type DefaultPost = {
+  __v: string;
+  _id: string;
+  author: string;
+  audience: string;
+  content: string;
+  background: string | null;
+  reactions: [];
+  createdAt: string;
+  updatedAt: string;
 };
