4f7088f0b9328cf4abfd49765eaee6e4661042d8
Erdinch <erdinchorhan02@gmail.com>
2023-02-11 23:15:18 +0200

    building image post

--- client/src/components/HomePage/Mobile/PostTypes/ImagePost.tsx
+++ client/src/components/HomePage/Mobile/PostTypes/ImagePost.tsx
@@ -1,15 +1,32 @@
+import { useEffect, useState } from 'react';
+import moment from 'moment';
 import { TDBPost } from '../../../../types/Post';
+import Pfp from '../../../../assets/pfp-two.svg';
 
 function ImagePost({ data }: { data: TDBPost }) {
-  console.log(data);
+  const [postDate, setPostDate] = useState<string | null>(null);
 
-  if (data.image) {
-    return (
-      <div>
-        <img alt="post" src={URL.createObjectURL(data.image.data)} />
+  useEffect(() => {
+    setPostDate(moment(new Date(data.createdAt)).format('MMM Do, YYYY'));
+  }, [data]);
+
+  return (
+    <>
+      <div className="flex gap-2">
+        <div className="w-12 h-12">
+          <Pfp height="100%" width="100%" />
+        </div>
+        <div className="flex flex-col">
+          <p className="font-bold">{data.author.fullName}</p>
+          <p>{postDate}</p>
+        </div>
       </div>
-    );
-  }
+      <div>
+        <p className="font-bold text-lg">{data.content}</p>
+        <img alt="post" src={data.image} />
+      </div>
+    </>
+  );
 }
 
 export default ImagePost;
