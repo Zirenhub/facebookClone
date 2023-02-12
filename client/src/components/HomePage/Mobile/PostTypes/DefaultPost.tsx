fbd3e3e66fa8a25d50c5e54e712253407019d8ea
Erdinch <erdinchorhan02@gmail.com>
2023-02-12 21:23:03 +0200

    building default post

--- client/src/components/HomePage/Mobile/PostTypes/DefaultPost.tsx
+++ client/src/components/HomePage/Mobile/PostTypes/DefaultPost.tsx
@@ -1,5 +1,11 @@
 import { TDBPost } from '../../../../types/Post';
 
-function DefaultPost({ data }: { data: TDBPost }) {}
+function DefaultPost({ data }: { data: TDBPost }) {
+  return (
+    <div>
+      <p>{data.content}</p>
+    </div>
+  );
+}
 
 export default DefaultPost;
