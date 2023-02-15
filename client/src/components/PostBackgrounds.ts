70c4116def0950475af9c4433df69db39ef690ef
Erdinch <erdinchorhan02@gmail.com>
2023-02-15 21:10:12 +0200

    post backgrounds seperate file

---
+++ client/src/components/PostBackgrounds.ts
@@ -0,0 +1,23 @@
+import postBgOne from '../../public/assets/post-bg-one.jpg';
+import postBgTwo from '../../public/assets/post-bg-two.jpg';
+import postBgThree from '../../public/assets/post-bg-three.jpg';
+import postBgFour from '../../public/assets/post-bg-four.jpg';
+
+type BackgroundsType = {
+  desc: string;
+  name: 'post-bg-one' | 'post-bg-two' | 'post-bg-three' | 'post-bg-four';
+  src: string;
+};
+
+const postBackgrounds: BackgroundsType[] = [
+  { desc: 'vinyl player background', name: 'post-bg-one', src: postBgOne },
+  { desc: 'animal eyes background', name: 'post-bg-two', src: postBgTwo },
+  { desc: 'rain drops background', name: 'post-bg-three', src: postBgThree },
+  {
+    desc: 'laughing emojis background',
+    name: 'post-bg-four',
+    src: postBgFour,
+  },
+];
+
+export default postBackgrounds;
