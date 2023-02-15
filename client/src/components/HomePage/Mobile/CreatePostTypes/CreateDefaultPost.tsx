85ced578283b55d0430aa95d16165dc2adbacafd
Erdinch <erdinchorhan02@gmail.com>
2023-02-15 21:11:59 +0200

    improved readability

--- client/src/components/HomePage/Mobile/CreatePostTypes/CreateDefaultPost.tsx
+++ client/src/components/HomePage/Mobile/CreatePostTypes/CreateDefaultPost.tsx
@@ -1,9 +1,6 @@
 import TextareaAutosize from 'react-textarea-autosize';
-import postBgOne from '../../../../../public/assets/post-bg-one.jpg';
-import postBgTwo from '../../../../../public/assets/post-bg-two.jpg';
-import postBgThree from '../../../../../public/assets/post-bg-three.jpg';
-import postBgFour from '../../../../../public/assets/post-bg-four.jpg';
 import { TPost } from '../../../../types/Post';
+import postBackgrounds from '../../../PostBackgrounds';
 
 type Props = {
   post: TPost;
@@ -11,35 +8,9 @@ type Props = {
 };
 
 function DefaultPost({ post, setPost }: Props) {
-  const postBackgrounds = [
-    { desc: 'vinyl player background', name: 'post-bg-one', src: postBgOne },
-    { desc: 'animal eyes background', name: 'post-bg-two', src: postBgTwo },
-    { desc: 'rain drops background', name: 'post-bg-three', src: postBgThree },
-    {
-      desc: 'laughing emojis background',
-      name: 'post-bg-four',
-      src: postBgFour,
-    },
-  ];
-
-  function getPostBackground() {
-    // `bg-[url('/assets/${post.background}.jpg')]` doesn't work for some reason
-    const bgClass = 'flex justify-center items-center';
-    switch (post.background) {
-      case 'post-bg-one':
-        return `bg-[url('/assets/post-bg-one.jpg')] ${bgClass} text-white`;
-      case 'post-bg-two':
-        return `bg-[url('/assets/post-bg-two.jpg')] ${bgClass} text-white`;
-      case 'post-bg-three':
-        return `bg-[url('/assets/post-bg-three.jpg')] ${bgClass} text-white`;
-      case 'post-bg-four':
-        return `bg-[url('/assets/post-bg-four.jpg')] ${bgClass} text-black`;
-      default:
-        return '';
-    }
-  }
-
-  function handlePostBackground(bg: string | null) {
+  function handlePostBackground(
+    bg: 'post-bg-one' | 'post-bg-two' | 'post-bg-three' | 'post-bg-four' | null
+  ) {
     if (post.content.length <= 250) {
       setPost({ ...post, background: bg });
     }
@@ -59,22 +30,44 @@ function DefaultPost({ post, setPost }: Props) {
     }
   }
 
+  function getPostBackground() {
+    const background = postBackgrounds.find((b) => b.name === post.background);
+    if (background) {
+      return (
+        <div className="relative">
+          <img
+            src={background.src}
+            alt={background.desc}
+            className="rounded-md"
+          />
+          <TextareaAutosize
+            className="bg-transparent absolute top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 text-white text-center w-full px-10 font-bold outline-none resize-none"
+            placeholder="What's on your mind?"
+            maxLength={450}
+            value={post.content}
+            onChange={(e) => handlePostContent(e)}
+          />
+        </div>
+      );
+    }
+    return null;
+  }
+
   return (
     <>
-      <div
-        className={`${getPostBackground()} bg-contain bg-center bg-no-repeat h-72`}
-      >
+      {post.background ? (
+        getPostBackground()
+      ) : (
         <TextareaAutosize
-          className={`bg-transparent font-bold outline-none resize-none ${
-            post.background ? 'text-center' : 'w-full'
-          }`}
+          className="w-full font-bold outline-none resize-none min-h-[200px]"
           placeholder="What's on your mind?"
           maxLength={450}
           value={post.content}
           onChange={(e) => handlePostContent(e)}
+          autoFocus
         />
-      </div>
-      <div className="flex w-full gap-2">
+      )}
+      <div className="flex w-full gap-2 mt-4">
         <button type="button" onClick={() => handlePostBackground(null)}>
           <div className="bg-gray-200 rounded-md h-12 w-16" />
         </button>
