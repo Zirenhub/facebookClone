c8fe5e33b08c0ccb2bd47d0a93eade9095e7900b
Erdinch <erdinchorhan02@gmail.com>
2023-02-19 19:13:56 +0200

    get api errors

--- client/src/api/getError.ts
+++ client/src/api/getError.ts
@@ -1,5 +1,27 @@
-function getError(data: any) {
-  return data.message ? data.message : data.errors.message;
+type ValidationError = {
+  location: string;
+  msg: string;
+  param: string;
+  value: string;
+};
+
+function getFinal(
+  status: 'success' | 'error',
+  data: any,
+  errors: ValidationError[] | null | undefined,
+  message: string | null
+) {
+  if (status === 'success') {
+    return data;
+  }
+  if (status === 'error' && message) {
+    return Promise.reject(new Error(message));
+  }
+  if (errors) {
+    const errorsString = new Error(errors.map((e) => e.msg).join('\n'));
+    return Promise.reject(errorsString);
+  }
+  return Promise.reject(new Error('Something went wrong'));
 }
 
-export default getError;
+export default getFinal;
