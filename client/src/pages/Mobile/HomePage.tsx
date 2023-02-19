7c6f6657de4cd45e2f48b001156e944ea439615c
Erdinch <erdinchorhan02@gmail.com>
2023-02-19 19:17:04 +0200

    delete mutation for posts

--- client/src/pages/Mobile/HomePage.tsx
+++ client/src/pages/Mobile/HomePage.tsx
@@ -1,17 +1,33 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useEffect, useState } from 'react';
 import Loading from '../../components/Loading';
 import MobileWritePost from '../../components/HomePage/Mobile/WritePost';
 import MobileAddStory from '../../components/HomePage/Mobile/AddStory';
-import { getPosts } from '../../api/post';
+import { deletePost, getPosts } from '../../api/post';
 import { TDBPost } from '../../types/Post';
 import SingularPost from '../../components/HomePage/Mobile/SingularPost';
 
 function HomePage() {
+  const [posts, setPosts] = useState<TDBPost[]>([]);
+
   const { isLoading, isError, data, error } = useQuery<TDBPost[], Error>({
-    queryKey: ['posts'],
+    queryKey: ['posts', 'timeline'],
     queryFn: () => getPosts(),
     refetchOnWindowFocus: false,
   });
+
+  const mutationDeletePost = useMutation({
+    mutationFn: (postID: string) => {
+      return deletePost(postID);
+    },
+    onSuccess(successData, variables, context) {
+      setPosts(posts.filter((post) => post._id !== variables));
+    },
+  });
+
+  useEffect(() => {
+    if (data) setPosts(data);
+  }, [data]);
 
   if (isLoading) {
     return <Loading />;
@@ -19,11 +35,11 @@ function HomePage() {
 
   return (
     <div className="p-2">
-      <MobileWritePost />
+      <MobileWritePost setPosts={setPosts} posts={posts} />
       <MobileAddStory />
-      {data && (
+      {posts && (
         <div>
-          {data
+          {posts
             .sort(
               (a, b) =>
                 new Date(b.createdAt).valueOf() -
@@ -35,7 +51,7 @@ function HomePage() {
                   key={post._id}
                   className="mt-2 border-b-4 border-slate-400"
                 >
-                  <SingularPost post={post} />
+                  <SingularPost post={post} deletePost={mutationDeletePost} />
                 </div>
               );
             })}
