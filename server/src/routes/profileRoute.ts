908113413efc4f78f65107ed0e2f297d6595ef8f
Erdinch <erdinchorhan02@gmail.com>
2023-02-19 19:18:05 +0200

    get posts route for profile

--- server/src/routes/profileRoute.ts
+++ server/src/routes/profileRoute.ts
@@ -7,6 +7,7 @@ import {
   acceptRequest,
   rejectRequest,
   getFriends,
+  getPosts,
 } from '../controllers/profileController';
 
 const router = express.Router();
@@ -17,5 +18,6 @@ router.post('/:id/reject', jwtAuth, rejectRequest);
 router.get('/requests', jwtAuth, getRequests);
 router.get('/friends', jwtAuth, getFriends);
 router.get('/:id', jwtAuth, getProfile);
+router.get('/:id/posts', jwtAuth, getPosts);
 
 export default router;
