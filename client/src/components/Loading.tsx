fc052c02c85b539e6587a6d2f3344e673c9f2bcc
Erdinch <erdinchorhan02@gmail.com>
2023-02-23 20:22:05 +0200

    loading screen z index priority

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,6 +1,6 @@
 function Loading() {
   return (
-    <div className="absolute top-0 left-0 h-full w-full flex justify-center items-center bg-gray-600">
+    <div className="absolute z-50 top-0 left-0 h-full w-full flex justify-center items-center bg-gray-600">
       <p className="text-white">Loading...</p>
     </div>
   );
