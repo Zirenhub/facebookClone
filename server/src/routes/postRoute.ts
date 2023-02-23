0c63163c1bd25ba97bc5bb15001dd7267b0ec05a
Erdinch <erdinchorhan02@gmail.com>
2023-02-23 20:26:25 +0200

    comment, comments routes

--- server/src/routes/postRoute.ts
+++ server/src/routes/postRoute.ts
@@ -7,17 +7,22 @@ import {
   likePost,
   unlikePost,
   deletePost,
+  postComment,
+  getPostComments,
 } from '../controllers/postController';
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
 router.post('/', jwtAuth, upload.single('image'), createPost);
+router.get('/:id', jwtAuth, getPost);
+router.delete('/:id', jwtAuth, deletePost);
+
 router.post('/:id/like', jwtAuth, likePost);
 router.post('/:id/unlike', jwtAuth, unlikePost);
-router.post('/:id/delete', jwtAuth, deletePost);
 
-router.get('/:id', jwtAuth, getPost);
+router.post('/:id/comment', jwtAuth, postComment);
+router.get('/:id/comments', jwtAuth, getPostComments);
 // update post ?
 
 export default router;
