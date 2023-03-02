103e6390a4b758e26c72bdb0764038837938d580
Erdinch <erdinchorhan02@gmail.com>
2023-03-02 19:32:45 +0200

    sort comments children, mutate additions

--- client/src/hooks/useComments.ts
+++ client/src/hooks/useComments.ts
@@ -1,25 +1,59 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
-import { getPostComments, likeComment, postComment } from '../api/post';
-import { TComment } from '../types/Post';
+import {
+  getPostComments,
+  likeComment,
+  postComment,
+  replyToComment,
+  unlikeComment,
+} from '../api/post';
+import { TComment, UpdatedComment } from '../types/Post';
+import useAuthContext from './useAuthContext';
 
 function useComments(postID: string) {
-  const [comments, setComments] = useState<TComment[]>([]);
+  const [comments, setComments] = useState<UpdatedComment[]>([]);
+  const [replyingTo, setReplyingTo] = useState<UpdatedComment | null>(null);
+
+  const auth = useAuthContext();
 
   const { isLoading, isError, error } = useQuery<TComment[], Error>({
     queryKey: ['comments', postID],
     queryFn: () => getPostComments(postID),
     onSuccess(successData) {
-      setComments(successData);
+      const nestedComments = successData.filter((c) => c.parent);
+      const sortedChildren = successData.map((p) => {
+        const children = nestedComments.filter((c) => c.parent === p._id);
+        return { ...p, children };
+      });
+      const removeChildren = sortedChildren.filter((c) => {
+        return !c.parent;
+      });
+      setComments(removeChildren);
     },
   });
 
-  const mutateSendComment = useMutation({
-    mutationFn: (commentID: string) => {
-      return postComment(postID, commentID);
+  const mutateReply = useMutation({
+    mutationFn: (comment: string) => {
+      if (replyingTo) {
+        return replyToComment(postID, replyingTo._id, comment);
+      }
+      return postComment(postID, comment);
     },
-    onSuccess(successData) {
-      setComments([...comments, successData]);
+    onSuccess(data) {
+      if (data.parent) {
+        const updatedComments = comments.map((c) => {
+          if (c._id === data.parent) {
+            return {
+              ...c,
+              children: [...c.children, { ...data, children: [] }],
+            };
+          }
+          return { ...c };
+        });
+        setComments(updatedComments);
+      } else {
+        setComments([...comments, { ...data, children: [] }]);
+      }
     },
   });
 
@@ -40,13 +74,36 @@ function useComments(postID: string) {
     },
   });
 
+  const mutateUnlikeComment = useMutation({
+    mutationFn: (commentID: string) => {
+      return unlikeComment(commentID);
+    },
+    onSuccess(data, variables) {
+      const updatedComments = comments.map((c) => {
+        if (c._id === variables) {
+          const updatedReactions = c.reactions.filter(
+            (r) => r.author !== auth.user?._id
+          );
+          return { ...c, reactions: updatedReactions };
+        }
+
+        return { ...c };
+      });
+
+      setComments(updatedComments);
+    },
+  });
+
   return {
     comments,
-    mutateSendComment,
+    mutateReply,
     mutateLikeComment,
+    mutateUnlikeComment,
     isLoading,
     isError,
     error,
+    replyingTo,
+    setReplyingTo,
   };
 }
 
