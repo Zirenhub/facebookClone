39f136c27a63ecc9fafca99698e46d3fbd7f1f1c
Erdinch <erdinchorhan02@gmail.com>
2023-03-02 16:31:25 +0200

    comment like btn, num of likes display

--- client/src/components/HomePage/Mobile/Post/SingleComment.tsx
+++ client/src/components/HomePage/Mobile/Post/SingleComment.tsx
@@ -1,16 +1,56 @@
+import { UseMutationResult } from '@tanstack/react-query';
 import moment from 'moment';
 import { useEffect, useState } from 'react';
 import Pfp from '../../../../assets/pfp-two.svg';
-import { Comment } from '../../../../types/Post';
+import { TComment } from '../../../../types/Post';
+import Like from '../../../../assets/like.svg';
+import useAuthContext from '../../../../hooks/useAuthContext';
 
-function SingleComment({ comment }: { comment: Comment }) {
+type Props = {
+  comment: TComment;
+  mutateLikeComment: UseMutationResult<any, unknown, string, unknown>;
+};
+
+function SingleComment({ comment, mutateLikeComment }: Props) {
   const [dateAgo, setDateAgo] = useState<string | null>(null);
+  const [numLikes, setNumLikes] = useState<number>(0);
+  const [commentIsLiked, setCommentIsLiked] = useState<boolean>(false);
+
+  const auth = useAuthContext();
 
   useEffect(() => {
     const commentDate = new Date(comment.createdAt);
     const ago = moment(commentDate).fromNow();
+
+    const commentLikes = comment.reactions.length;
+    const likedComment = comment.reactions.find(
+      (c) => c.author === auth.user?._id
+    );
+
     setDateAgo(ago);
-  }, [comment]);
+    setNumLikes(commentLikes);
+    if (likedComment) {
+      setCommentIsLiked(true);
+    }
+  }, [comment, auth.user]);
+
+  function getLikedButton() {
+    if (commentIsLiked) {
+      return (
+        <button type="button" className="text-blue-500">
+          Unlike
+        </button>
+      );
+    }
+    return (
+      <button
+        type="button"
+        onClick={() => mutateLikeComment.mutate(comment._id)}
+      >
+        Like
+      </button>
+    );
+  }
 
   return (
     <>
@@ -24,8 +64,12 @@ function SingleComment({ comment }: { comment: Comment }) {
         </div>
         <div className="flex text-dimGray gap-3">
           <p>{dateAgo}</p>
-          <button type="button">Like</button>
+          {getLikedButton()}
           <button type="button">Reply</button>
+          <div className="ml-auto mr-2 flex items-center">
+            <p>{numLikes}</p>
+            <Like fill="white" />
+          </div>
         </div>
       </div>
     </>
