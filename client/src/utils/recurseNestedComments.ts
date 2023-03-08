c23b9050bd8aadc01eb7b6ddb4ab11a2878b6b93
Erdinch <erdinchorhan02@gmail.com>
2023-03-08 22:44:24 +0200

    refactored recursive addcomment

--- client/src/utils/recurseNestedComments.ts
+++ client/src/utils/recurseNestedComments.ts
@@ -1,31 +1,5 @@
 import { NestedComment, TComment } from '../types/Post';
 
-function findCommentInChildren(
-  children: NestedComment[],
-  parentId: string
-): NestedComment | null {
-  for (let i = 0; i < children.length; i += 1) {
-    const child = children[i];
-
-    if (child._id === parentId) {
-      // parent comment is found in this level of children comments
-      return child;
-    }
-
-    // parent comment may be in grandchildren comments, recursively check each grandchild comment
-    if (child.children) {
-      const parentComment = findCommentInChildren(child.children, parentId);
-
-      if (parentComment) {
-        return parentComment;
-      }
-    }
-  }
-
-  // parent comment is not found in any level of children comments
-  return null;
-}
-
 function addComment(newComments: NestedComment[], comments: TComment[]) {
   for (let i = 0; i < comments.length; i += 1) {
     const comment = comments[i];
@@ -41,21 +15,12 @@ function addComment(newComments: NestedComment[], comments: TComment[]) {
         return true;
       }
 
-      // parent comment may be in children comments, recursively check each child comment
-      if (comment.children) {
-        const parentComment = findCommentInChildren(comment.children, c.parent);
-
-        if (parentComment) {
-          if (!comment.children) {
-            comment.children = [];
-          }
-
-          comment.children.push(c);
-          return true;
-        }
-      }
       return false;
     });
+    // parent comment may be in children comments, recursively check each child comment
+    if (comment.children) {
+      addComment(newComments, comment.children);
+    }
   }
 
   return comments;
