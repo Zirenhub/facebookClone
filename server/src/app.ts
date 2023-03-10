b22f0cfaf8bca29a3599ebed81b2075429445288
Erdinch <erdinchorhan02@gmail.com>
2023-03-10 21:07:35 +0200

    socket io

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express, { Express } from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import http from 'http';
+import { Server } from 'socket.io';
 
 import authRoute from './routes/authRoute';
 import postRoute from './routes/postRoute';
@@ -9,6 +11,7 @@ import profileRoute from './routes/profileRoute';
 import searchRoute from './routes/searchRoute';
 import timelineRoute from './routes/timelineRoute';
 import commentRoute from './routes/commentRoute';
+import { jwtAuth } from './middleware/jwtAuth';
 
 dotenv.config();
 
@@ -18,17 +21,36 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+const server = http.createServer(app);
+const io = new Server(server, {
+  cookie: true,
+});
+
 const port = process.env.PORT || 5000;
 
 app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/post', postRoute);
-app.use('/api/v1/profile', profileRoute);
-app.use('/api/v1/search', searchRoute);
-app.use('/api/v1/timeline', timelineRoute);
-app.use('/api/v1/comment', commentRoute);
+app.use('/api/v1/post', jwtAuth, postRoute);
+app.use('/api/v1/profile', jwtAuth, profileRoute);
+app.use('/api/v1/search', jwtAuth, searchRoute);
+app.use('/api/v1/timeline', jwtAuth, timelineRoute);
+app.use('/api/v1/comment', jwtAuth, commentRoute);
 
 mongoose.connect(process.env.DB_URI!).then(() => {
-  app.listen(port, () =>
+  io.on('connection', (socket) => {
+    const cookie = socket.request;
+    console.log(cookie);
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    socket.on('message', (msg) => {
+      console.log('message', msg);
+    });
+  });
+
+  server.listen(port, () =>
     console.log(`Server is running at http://localhost:${port}`)
   );
 });
