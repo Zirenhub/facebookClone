6b944ed537eb61b4afb921f4c5fb32394d1149c1
Erdinch <erdinchorhan02@gmail.com>
2023-03-18 22:54:45 +0200

    removed online route

--- client/src/api/online.ts
+++
@@ -1,7 +0,0 @@
-import getFinal from './getError';
-
-export default async function getOnlineIds(): Promise<string[]> {
-  const res = await fetch('/api/v1/online/');
-  const { status, data, errors, message } = await res.json();
-  return getFinal(status, data, errors, message);
-}
