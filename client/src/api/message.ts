d6d83a030ff25fecf45e1ac1fec49c58a815e69e
Erdinch <erdinchorhan02@gmail.com>
2023-03-18 22:54:54 +0200

    send message

--- client/src/api/message.ts
+++ client/src/api/message.ts
@@ -1,11 +1,22 @@
-import { GetMessagesRes } from '../types/Api';
+import { GetMessagesRes, SendMessageRes } from '../types/Api';
 import TMessage from '../types/Message';
 import getFinal from './getError';
 
-export default async function getMessages(
-  partnerID: string
-): Promise<TMessage[]> {
+export async function getMessages(partnerID: string): Promise<TMessage[]> {
   const res = await fetch(`/api/v1/messages/${partnerID}/`);
   const { status, data, errors, message }: GetMessagesRes = await res.json();
+  return getFinal(status, data, errors, message);
+}
+
+export async function sendMessage(
+  receiver: string,
+  messageContent: string
+): Promise<TMessage> {
+  const res = await fetch('/api/v1/messages', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ receiver, message: messageContent }),
+  });
+  const { status, data, errors, message }: SendMessageRes = await res.json();
   return getFinal(status, data, errors, message);
 }
