59b83d9e61e26ab199f473ac841b6c2d35fd660d
Erdinch <erdinchorhan02@gmail.com>
2023-03-25 23:11:28 +0200

    show online friends

--- client/src/pages/Mobile/Messenger.tsx
+++ client/src/pages/Mobile/Messenger.tsx
@@ -1,14 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getFriends } from '../../api/profile';
 import Close from '../../assets/x.svg';
 import Loading from '../../components/Loading';
 import { TProfileDefault } from '../../types/Profile';
 import Pfp from '../../assets/pfp-two.svg';
 import Chat from '../../components/Chat/Chat';
+import useSocketContext from '../../hooks/useSocketContext';
 
 function Messenger({ close }: { close: () => void }) {
   const [currentChat, setCurrentChat] = useState<TProfileDefault | null>(null);
+  const [onlineFriends, setOnlineFriends] = useState<string[] | null>(null);
+
+  const soc = useSocketContext();
 
   const { isLoading, isError, data, error } = useQuery<
     TProfileDefault[],
@@ -17,6 +21,12 @@ function Messenger({ close }: { close: () => void }) {
     queryKey: ['friends'],
     queryFn: () => getFriends(),
   });
+
+  useEffect(() => {
+    soc.getOnlineFriends((friends) => {
+      setOnlineFriends(friends);
+    });
+  }, [soc]);
 
   if (isLoading) {
     <Loading />;
@@ -39,23 +49,25 @@ function Messenger({ close }: { close: () => void }) {
       <button type="button" onClick={close} className="h-6 w-6">
         <Close height="100%" width="100%" />
       </button>
-      <div className="mt-5">
+      <div className="flex flex-col">
         {data?.map((friend) => {
+          const isOnline = onlineFriends?.includes(friend._id);
+
           return (
-            <div
+            <button
               key={friend._id}
-              className="flex font-bold gap-3 border-b-2 pb-2"
-              role="button"
-              tabIndex={0}
+              type="button"
+              className="flex font-bold gap-3 border-b-2 pb-2 grow my-2"
               onClick={() => setCurrentChat(friend)}
-              onKeyDown={() => setCurrentChat(friend)}
             >
               <div className="h-12 w-12 relative">
                 <Pfp height="100%" width="100%" />
-                <div className="absolute h-3 w-3 border-2 border-white bg-green-400 bottom-1 right-1 rounded-full" />
+                {isOnline && (
+                  <div className="absolute h-3 w-3 border-2 border-white bg-green-400 bottom-1 right-1 rounded-full" />
+                )}
               </div>
               <p className="text-xl">{friend.fullName}</p>
-            </div>
+            </button>
           );
         })}
       </div>
