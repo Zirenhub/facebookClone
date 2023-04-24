b57031a66c00d400ab8387d5cb4a518d3d3aae40
Erdinch <erdinchorhan02@gmail.com>
2023-04-25 00:14:12 +0300

    refactored to also work with desktop

--- client/src/components/Posts/CreateImagePost.tsx
+++ client/src/components/Posts/CreateImagePost.tsx
@@ -1,42 +1,67 @@
 import TextareaAutosize from 'react-textarea-autosize';
+import { useRef } from 'react';
 import { TPost } from '../../types/Post';
 
 type Props = {
   post: TPost;
   handlePostContent: (e: React.SyntheticEvent<Element, Event>) => void;
   handleImageChange: (e: React.SyntheticEvent<Element, Event>) => void;
+  handleRemoveImage: () => void;
 };
 
-function ImagePost({ post, handlePostContent, handleImageChange }: Props) {
+function ImagePost({
+  post,
+  handlePostContent,
+  handleImageChange,
+  handleRemoveImage,
+}: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <>
+    <div>
       <TextareaAutosize
-        className="font-bold outline-none resize-none placeholder:text-gray-600 w-full"
+        className="outline-none text-lg resize-none placeholder:text-dimGray w-full"
         placeholder="Say something about this image."
         maxLength={post.maxLength}
         value={post.content}
         onChange={(e) => handlePostContent(e)}
       />
-      <div className="w-full flex justify-center items-center bg-gray-200">
-        {post.image ? (
+      {!post.image && (
+        <button
+          type="button"
+          onClick={() => inputRef.current?.click()}
+          className="w-full bg-gray-100 rounded-md py-5 mt-10 border-2 hover:bg-gray-200 transition-all"
+        >
+          Choose Image
+        </button>
+      )}
+      {post.image && (
+        <div className="w-full flex justify-center items-center mt-5 relative">
+          <button
+            type="button"
+            onClick={handleRemoveImage}
+            className="absolute top-2 right-2 bg-white rounded-full px-1"
+          >
+            <span className="text-dimBlack">&#10006;</span>
+          </button>
           <img
             alt="your file"
             src={URL.createObjectURL(post.image)}
-            className="max-h-64"
+            className="max-h-64 rounded-lg"
           />
-        ) : (
-          <p>Please select an image.</p>
-        )}
-      </div>
+        </div>
+      )}
       <form className="pt-2" encType="multipart/form-data">
         <input
+          ref={inputRef}
           type="file"
+          accept="image/png, image/jpeg"
           onChange={handleImageChange}
           name="post-image"
-          accept="image/png, image/jpeg"
+          style={{ display: 'none' }}
         />
       </form>
-    </>
+    </div>
   );
 }
 
