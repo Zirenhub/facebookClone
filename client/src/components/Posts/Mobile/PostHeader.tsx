75d9ae015d3d1d0750515140ef175dd65bbde983
Erdinch <erdinchorhan02@gmail.com>
2023-04-25 23:02:06 +0300

    improved header code, 3 seperate divs instead of one.

--- client/src/components/Posts/Mobile/PostHeader.tsx
+++ client/src/components/Posts/Mobile/PostHeader.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import moment from 'moment';
 import { ModifiedPost } from '../../../types/Post';
 import Pfp from '../../../assets/pfp-two.svg';
@@ -7,6 +7,7 @@ import stringShortener from '../../../utils/stringShortener';
 
 type Props = {
   post: ModifiedPost;
+  setOpenPost?: () => void;
   mutationDeletePost?: {
     isLoading: boolean;
     isError: boolean;
@@ -15,7 +16,7 @@ type Props = {
   };
 };
 
-function PostHeader({ post, mutationDeletePost }: Props) {
+function PostHeader({ post, setOpenPost, mutationDeletePost }: Props) {
   const [postDate, setPostDate] = useState<string | null>(null);
   const [settingsMenu, setSettingsMenu] = useState<boolean>(false);
 
@@ -26,18 +27,41 @@ function PostHeader({ post, mutationDeletePost }: Props) {
     navigate(`/${post.author._id}`);
   }
 
+  function deletePost(e: React.SyntheticEvent) {
+    e.stopPropagation();
+    if (mutationDeletePost) mutationDeletePost.deletePost(post._id);
+  }
+
+  function handlePostMenu(e: React.SyntheticEvent) {
+    e.stopPropagation();
+    setSettingsMenu(!settingsMenu);
+  }
+
+  function switchToFullPost(e: React.SyntheticEvent) {
+    e.stopPropagation();
+    if (setOpenPost) {
+      setOpenPost();
+    }
+  }
+
   useEffect(() => {
     setPostDate(moment(new Date(post.createdAt)).format('MMM Do, YYYY'));
   }, [post]);
 
   return (
-    <div className="flex relative">
+    <div
+      className="flex items-center justify-between"
+      onKeyDown={navigateProfile}
+      onClick={switchToFullPost}
+      role="button"
+      tabIndex={0}
+    >
       <div
         onClick={navigateProfile}
         onKeyDown={navigateProfile}
         role="button"
         tabIndex={0}
-        className="flex gap-3 grow"
+        className="flex gap-3"
       >
         <div className="w-12 h-12">
           <Pfp height="100%" width="100%" />
@@ -49,44 +73,34 @@ function PostHeader({ post, mutationDeletePost }: Props) {
             </p>
             <p>{postDate}</p>
           </div>
-          {post.audience === 'public' && (
-            <p className="ml-auto text-dimGray">Public</p>
-          )}
-          {mutationDeletePost && (
-            <button
-              type="button"
-              onClick={(e: React.SyntheticEvent) => {
-                e.stopPropagation();
-                setSettingsMenu(!settingsMenu);
-              }}
-              className="text-3xl h-fit"
-            >
-              &#8943;
-            </button>
-          )}
         </div>
       </div>
-
-      {settingsMenu && (
-        <div className="bg-white rounded-md border-2 px-3 py-2 absolute right-2 top-12 z-10 flex flex-col">
+      <div className="relative flex items-center gap-3 text-dimGray">
+        {post.audience === 'public' && <p className="ml-auto">Public</p>}
+        {mutationDeletePost && (
           <button
             type="button"
-            onClick={(e: React.SyntheticEvent) => {
-              e.stopPropagation();
-              if (mutationDeletePost) mutationDeletePost.deletePost(post._id);
-            }}
-            className="border-b-2"
+            onClick={handlePostMenu}
+            className="text-3xl h-fit"
           >
-            Delete
+            &#8943;
           </button>
-          <button type="button">Update</button>
-        </div>
-      )}
+        )}
+        {settingsMenu && (
+          <div className="bg-white rounded-md border-2 px-3 py-2 absolute right-2 top-10 z-10 flex flex-col">
+            <button type="button" onClick={deletePost} className="border-b-2">
+              Delete
+            </button>
+            <button type="button">Update</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
 PostHeader.defaultProps = {
+  setOpenPost: undefined,
   mutationDeletePost: undefined,
 };
 
