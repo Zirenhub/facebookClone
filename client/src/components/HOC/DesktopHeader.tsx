3408712b61a822da32e6e511803322c7963189e6
Erdinch <erdinchorhan02@gmail.com>
2023-04-28 22:50:58 +0300

    desktop header refactored to single state

--- client/src/components/HOC/DesktopHeader.tsx
+++ client/src/components/HOC/DesktopHeader.tsx
@@ -5,20 +5,22 @@ import Messenger from '../../assets/messenger.svg';
 import Menu from '../../assets/menu.svg';
 import Pfp from '../../assets/pfp-two.svg';
 import Bell from '../../assets/bell.svg';
-
-import DesktopMenu from '../../pages/Desktop/Menu';
+import DesktopMenu from './Menu';
 import DesktopSearch from './DesktopSearch';
+import HeaderProfile from './HeaderProfile';
+
+type TModal = 'search' | 'menu' | 'profile';
 
 function Header() {
-  const [menu, setMenu] = useState<boolean>(false);
-  const [search, setSearch] = useState<boolean>(false);
+  const [currentModal, setCurrentModal] = useState<TModal | null>(null);
 
   const mainRef = useRef<HTMLDivElement>(null);
+  const headerBtnClass =
+    'bg-gray-200 h-full w-10 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300';
 
   useEffect(() => {
     function closePopups() {
-      setSearch(false);
-      setMenu(false);
+      setCurrentModal(null);
     }
     const { current } = mainRef;
     current?.addEventListener('click', closePopups);
@@ -27,14 +29,41 @@ function Header() {
     };
   }, []);
 
+  function headerButton(modal: 'menu' | 'profile') {
+    return (
+      <button
+        type="button"
+        className={`${headerBtnClass} ${
+          currentModal === modal ? 'bg-blue-200 hover:bg-blue-300' : ''
+        } ${modal === 'menu' ? 'p-2' : ''}`}
+        onClick={() => setCurrentModal(currentModal === modal ? null : modal)}
+      >
+        {modal === 'menu' ? (
+          <Menu
+            height="100%"
+            width="100%"
+            fill={currentModal === 'menu' ? 'blue' : ''}
+          />
+        ) : (
+          <Pfp height="100%" width="100%" />
+        )}
+      </button>
+    );
+  }
+
   const { pages, notifications, latestNotif, currentPage, setNotifications } =
     useRoute(false);
 
   return (
     <>
-      <header className="grid grid-cols-[1fr_2fr_1fr] px-3 shadow-lg z-30">
+      <header className="grid grid-cols-[1fr_2fr_1fr] px-3 shadow-lg z-30 relative">
         <div className="flex items-center h-full gap-3 py-2">
-          <DesktopSearch isOpen={!!search} setSearch={setSearch} />
+          <DesktopSearch
+            isOpen={currentModal === 'search'}
+            setSearch={(bool: boolean) =>
+              setCurrentModal(bool ? 'search' : null)
+            }
+          />
         </div>
         <div className="flex justify-between items-center">
           {pages.map((page) => {
@@ -57,28 +86,21 @@ function Header() {
           })}
         </div>
         <div className="flex justify-end items-center gap-2 py-2">
-          <button
-            type="button"
-            className={`h-full w-10 p-2 rounded-full flex justify-center items-center cursor-pointer  ${
-              menu
-                ? 'bg-blue-200 hover:bg-blue-300'
-                : 'bg-gray-200 hover:bg-gray-300'
-            }`}
-            onClick={() => setMenu(!menu)}
-          >
-            <Menu height="100%" width="100%" fill={menu ? 'blue' : ''} />
-          </button>
-          <div className="bg-gray-200 h-full w-10 p-2 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300">
+          {headerButton('menu')}
+          <div className={`${headerBtnClass} p-2`}>
             <Messenger height="100%" width="100%" />
           </div>
-          <div className="bg-gray-200 h-full w-10 p-2 rounded-full flex justify-center items-center cursor-pointer hover:bg-gray-300">
+          <div className={`${headerBtnClass} p-2`}>
             <Bell height="100%" width="100%" />
           </div>
-          <div className="h-full w-10 rounded-full cursor-pointer">
-            <Pfp height="100%" width="100%" />
-          </div>
+          {headerButton('profile')}
         </div>
-        {menu && <DesktopMenu />}
+        {currentModal && (
+          <div className="absolute top-12 right-2">
+            {currentModal === 'menu' && <DesktopMenu />}
+            {currentModal === 'profile' && <HeaderProfile />}
+          </div>
+        )}
       </header>
       <main className="overflow-auto grow" ref={mainRef}>
         <Outlet
